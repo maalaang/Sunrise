0242613ace28cfdc19d8f4f4ea8cb7e436b95680
Sangwon Park <blackhat@maalaang.com>
2013-07-10 13:14:16 +0900

    Updated WebSocket client testing page

--- msg/client.php
+++ msg/client.php
@@ -1,58 +1,106 @@
-<html>
+t<html>
 <head>
-<title>WebSocket</title>
+<title>Sunrise - WebSocket Client Test</title>
 
 <style>
-html,body{font:normal 0.9em arial,helvetica;}
-#log {width:440px; height:200px; border:1px solid #7F9DB9; overflow:auto;}
-#msg {width:330px;}
+html,body {
+    font:normal 0.9em arial,helvetica;
+}
+#log {
+    width:500px;
+    height:800px;
+    border:1px solid gray;
+    overflow:auto;
+}
+#msg {
+    width:400px;
+}
 </style>
 
 <script>
 var socket;
+var host = "ws://dev.maalaang.com:8889/echo";
 
-function init(){
-    var host = "ws://dev.maalaang.com:8889/echo";
-    try{
+function init() {
+    try {
         socket = new WebSocket(host);
-        log('WebSocket - status '+socket.readyState);
-        socket.onopen    = function(msg){ log("Welcome - status "+this.readyState); };
-        socket.onmessage = function(msg){ log("Received: "+msg.data); };
-        socket.onclose   = function(msg){ log("Disconnected - status "+this.readyState); };
+        log("Connecting to the server..");
+        socket.onopen = onOpen;
+        socket.onmessage = onMessage;
+        socket.onclose = onClose;
+        socket.onerror = onError;
+    } catch (ex) {
+        log("Exception: " + ex);
     }
-    catch(ex){ log(ex); }
     $("msg").focus();
 }
 
 function send(){
-    var txt,msg;
-    txt = $("msg");
-    msg = txt.value;
-    if(!msg){ alert("Message can not be empty"); return; }
+    var txt = $("msg");
+    var msg = txt.value;
+    if (!msg) {
+        return;
+    }
     txt.value="";
     txt.focus();
-    try{ socket.send(msg); log('Sent: '+msg); } catch(ex){ log(ex); }
-}
-function quit(){
-    log("Goodbye!");
-    socket.close();
-    socket=null;
+    try {
+        socket.send(msg);
+        log('>>> '+ msg);
+    } catch (ex) {
+        log(ex);
+    }
 }
 
-// Utilities
-function $(id){ return document.getElementById(id); }
-function log(msg){ $("log").innerHTML+="<br>"+msg; }
-function onkey(event){ if(event.keyCode==13){ send(); } }
+function exit(){
+    log("Closing the connection..");
+    socket.close();
+    socket = null;
+}
+
+
+function onOpen(message) {
+    if (this.readyState == 1) {
+        log("Connected");
+    } else {
+        log("Failed");
+    }
+}
+
+function onMessage(message) {
+    log("<<< " + message.data);
+}
+
+function onClose(event) {
+    log("Connection closed");
+}
+
+function onError(event) {
+    log("Error occurred: " + event);
+}
+
+function $(id) {
+    return document.getElementById(id);
+}
+
+function log(msg) {
+    $("log").innerHTML += "<br>" + msg;
+}
+
+function onkey(event) {
+    if (event.keyCode == 13) {
+        send();
+    }
+}
 </script>
 
 </head>
+
 <body onload="init()">
-<h3>WebSocket v2.00</h3>
-<div id="log"></div>
-<input id="msg" type="textbox" onkeypress="onkey(event)"/>
-<button onclick="send()">Send</button>
-<button onclick="quit()">Quit</button>
-<div>Commands: hello, hi, name, age, date, time, thanks, bye</div>
+    <h3>Sunrise - WebSocket Client Test</h3>
+    <div id="log"></div>
+    <input id="msg" type="textbox" onkeypress="onkey(event)"/>
+    <button onclick="send()">Send</button>
+    <button onclick="exit()">Exit</button>
 </body>
 </html>
 
