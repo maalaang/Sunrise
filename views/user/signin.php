f76d466b0325a14570acebde15837a7c61dd081e
Jeong-hyeon Kim <husky@maalaang.com>
2013-07-31 18:06:56 +0900

    Create signin.php & signup.php

---
+++ views/user/signin.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Sign in</title>
+    </head>
+    <body>
+        <h1>Sign in</h1>
+    </body>
+</html>
---
+++ views/user/signup.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Sign up</title>
+    </head>
+    <body>
+        <h1>Sign up</h1>
+    </body>
+</html>
