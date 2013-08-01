0a56a05ad873a9ac8f272c087a8656878170a985
husky <husky@maalaang.com>
2013-08-01 18:49:42 +0900

    Change form action

--- views/main/signin.php
+++ views/main/signin.php
@@ -1,25 +1,14 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <script type="text/javascript" src="<?= $GLOBALS['sr_root'] ?>/js/jquery-1.9.1.min.js"></script>
-        <script>
-            $(document).ready(function () {
-                $('input:text').bind('focus', function () {
-                    $(this).val('');
-                });
-                $('#password').bind('focus', function () {
-                    $(this).attr('type', 'password');
-                });
-            });
-        </script>
+
     </head>
     <body>
         <h1>Sign In</h1>
-        <form action="<?= $GLOBALS['sr_root'] ?>/models/user.php" name="signin_form" id="signin_form" method="post">
-            <input type="text" id="email" name="email" value="Email" /><br />
-            <input type="text" id="password" name="password" value="Password" /><br />
-            <input type="submit" value="Sign In" />
+        <form action="<?= $GLOBALS['sr_root_husky'] ?>/controllers/signin.php" name="signin_form" id="signin_form" method="post">
+            <input type="text" id="email" name="email" placeholder="Email" /><br />
+            <input type="password" id="password" name="password" placeholder="Password" /><br />
+            <input type="submit" id="submit" name="submit" value="Sign In" />
         </form>
     </body>
 </html>
--- views/main/signup.php
+++ views/main/signup.php
@@ -1,29 +1,16 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <script type="text/javascript" src="<?= $GLOBALS['sr_root'] ?>/js/jquery-1.9.1.min.js"></script>
-        <script>
-            $(document).ready(function () {
-                $('input:text').bind('focus', function () {
-                    $(this).val('');
-                });
-                $('#pw > *').bind('focus', function () {
-                    $(this).attr('type', 'password');
-                });
-            });
-        </script>
+
     </head>
     <body>
         <h1>Sign Up</h1>
-        <form action="<?= $GLOBALS['sr_root'] ?>/models/user.php" name="signup_form" id="signup_form" method="post">
-            <input type="text" id="first_name" name="first_name" value="First Name" />
-            <input type="text" id="last_name" name="last_name" value="Last Name" /><br />
-            <input type="text" id="email" name="email" value="Email" /><br />
-            <div id="pw">
-                <input type="text" id="password" name="password" value="Password" /><br />
-                <input type="text" id="repeat_password" name="repeat_password" value="Repeat Password" /><br />
-            </div>
+        <form action="<?= $GLOBALS['sr_root_husky'] ?>/controllers/signup.php" name="signup_form" id="signup_form" method="post">
+            <input type="text" id="first_name" name="first_name" placeholder="First Name" />
+            <input type="text" id="last_name" name="last_name" placeholder="Last Name" /><br />
+            <input type="text" id="email" name="email" placeholder="Email" /><br />
+            <input type="password" id="password" name="password" placeholder="Password" /><br />
+            <input type="password" id="repeat_password" name="repeat_password" placeholder="Repeat Password" /><br />
             <input type="submit" id="submit" name="submit" value="Sign Up" />
         </form>
     </body>
