68f3bf2de05fa04600fd295268da048285609199
husky <husky@maalaang.com>
2013-08-01 20:39:32 +0900

    While Working

--- controllers/signup.php
+++ controllers/signup.php
@@ -1,9 +1,9 @@
 <?php
 
 require_once (dirname(__FILE__) . '/../models/user.php');
+require_once (dirname(__FILE__) . '/../models/session.php');
+require_once (dirname(__FILE__) . '/../models/participant.php');
 require_once (dirname(__FILE__) . '/../include/utils.php');
-
-$user = new User();
 
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
@@ -11,14 +11,17 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 $repeat_password = $_POST['repeat_password'];
 
-$db = sr_pdo();
-$stmt = $db->prepares('SELECT * FROM user');
-$stmt->execute();
+try {
+    $db = sr_pdo();
 
-$query =
-    "INSERT INTO user (first_name, last_name, email, password)" .
-    "VALUES ('$first_name', '$last_name', '$email', '$password')";
+    $query =
+        "INSERT INTO user (first_name, last_name, email, password)" .
+        "VALUES ('$first_name', '$last_name', '$email', '$password')";
 
-$stmt = $db->query($query);
+    $stmt = $db->query($query);
+
+} catch (PDOException $e) {
+
+}
 
 ?>
