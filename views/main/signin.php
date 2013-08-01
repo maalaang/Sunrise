40ff058bcd943485905ff50a704ae0cc5b709da4
husky <husky@maalaang.com>
2013-08-01 20:44:31 +0900

    Clear conflict

--- views/main/signin.php
+++ views/main/signin.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-<<<<<<< HEAD
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
@@ -13,16 +12,6 @@
 
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <script type="text/javascript" src="<?= $GLOBALS['sr_root'] ?>/js/jquery-1.9.1.min.js"></script>
-        <script>
-            $(document).ready(function () {
-                $('input:text').bind('focus', function () {
-                    $(this).val('');
-                });
-                $('#password').bind('focus', function () {
-                    $(this).attr('type', 'password');
-                });
-            });
-        </script>
     </head>
     <body>
         <div class="navbar navbar-inverse navbar-fixed-top">
@@ -30,20 +19,10 @@
                 <a class="navbar-brand" href="#">Sunrise</a>
             </div>
         </div>
-        <form action="<?= $GLOBALS['sr_root'] ?>/models/user.php" name="signin_form" id="signin_form" method="post">
-            <input type="text" id="email" name="email" value="Email" /><br />
-            <input type="text" id="password" name="password" value="Password" /><br />
-            <input type="submit" value="Sign In" />
-=======
-
-    </head>
-    <body>
-        <h1>Sign In</h1>
         <form action="<?= $GLOBALS['sr_root_husky'] ?>/controllers/signin.php" name="signin_form" id="signin_form" method="post">
             <input type="text" id="email" name="email" placeholder="Email" /><br />
             <input type="password" id="password" name="password" placeholder="Password" /><br />
-            <input type="submit" id="submit" name="submit" value="Sign In" />
->>>>>>> 49c0975519ea3e678d3909cbd4d74aa68a880073
+            <input type="submit" value="Sign In" />
         </form>
     </body>
 </html>
