f2aa9b416286a70fbf834ba1beec0f7750b435ae
husky <husky@maalaang.com>
2013-08-02 14:32:40 +0900

    Use model.php's add() function

--- controllers/signup.php
+++ controllers/signup.php
@@ -1,24 +1,20 @@
 <?php
 
 require_once (dirname(__FILE__) . '/../models/user.php');
-require_once (dirname(__FILE__) . '/../models/session.php');
-require_once (dirname(__FILE__) . '/../models/participant.php');
 require_once (dirname(__FILE__) . '/../include/utils.php');
 
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$repeat_password = $_POST['repeat_password'];
+$user = new User();
+$user->first_name = $_POST['first_name'];
+$user->last_name = $_POST['last_name'];
+$user->email = $_POST['email'];
+$user->password = $_POST['password'];
+$user->is_authorized = 1;
+$user->join_date = $user->getCurrentTime();
+$user->last_active_date = $user->getCurrentTime();
 
 try {
     $db = sr_pdo();
-
-    $query =
-        "INSERT INTO user (first_name, last_name, email, password)" .
-        "VALUES ('$first_name', '$last_name', '$email', '$password')";
-
-    $stmt = $db->query($query);
+    $user->add($db);
 
 } catch (PDOException $e) {
 
