801e86ba7c821f62ac180bcb87b037b0c448a037
husky <husky@maalaang.com>
2013-08-02 18:47:18 +0900

    Modify Signup Code

--- controllers/signup.php
+++ controllers/signup.php
@@ -4,13 +4,14 @@ require_once (dirname(__FILE__) . '/../models/user.php');
 require_once (dirname(__FILE__) . '/../include/utils.php');
 
 $user = new User();
+
 $user->first_name = $_POST['first_name'];
 $user->last_name = $_POST['last_name'];
 $user->email = $_POST['email'];
 $user->password = $_POST['password'];
 $user->is_authorized = 1;
-$user->join_date = $user->getCurrentTime();
-$user->last_active_date = $user->getCurrentTime();
+$user->join_date = Model::getCurrentTime();
+$user->last_active_date = Model::getCurrentTime();
 
 $db = sr_pdo();
 
