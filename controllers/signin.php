831cab4f69c861988f3c83e5fe9aacbe07103705
husky <husky@maalaang.com>
2013-08-02 18:46:55 +0900

    Beginning Signin Process

--- controllers/signin.php
+++ controllers/signin.php
@@ -1,5 +1,39 @@
 <?php
 
-echo 'Sign in Page';
+require_once (dirname(__FILE__) . '/../models/user.php');
+require_once (dirname(__FILE__) . '/../include/utils.php');
+
+if (fetchUser($user)) {
+    echo 'Login Success';
+}
+
+function fetchUser($user) {
+    $db = sr_pdo();
+
+    try {
+        $stmt = $db->prepare('SELECT * FROM user WHERE email = :email');
+        $stmt->bindParam(':email', $_POST['email']);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, 'User');
+        $stmt->execute();
+
+        $user = $stmt->fetch();
+
+    } catch (PDOException $e) {
+        echo 'Exception';
+        return false;
+    }
+
+    if ($user == Null || $user == false) {
+        echo 'Couldn\'t Find Email Address';
+        return false;
+    } 
+
+    if ($user->password != $_POST['password']) {
+        echo 'Wrong Password';
+        return false;
+    }
+    
+    return true;
+}
 
 ?>
