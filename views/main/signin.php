b85fddce73aa7c9dcddcf17a012061ed94dbbfa5
whale <whale@maalaang.com>
2013-08-02 15:08:23 +0900

    signin Page

--- views/main/signin.php
+++ views/main/signin.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-<<<<<<< HEAD
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
@@ -9,7 +8,7 @@
 
         <title>Sunrise</title>
 
-        <link href="../css/bootstrap.css" rel="stylesheet">
+        <link href="/workspace/whale/Sunrise/css/bootstrap.css" rel="stylesheet">
 
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <script type="text/javascript" src="<?= $GLOBALS['sr_root'] ?>/js/jquery-1.9.1.min.js"></script>
@@ -23,6 +22,15 @@
                 });
             });
         </script>
+        <style>
+            body {
+                margin: 100px;
+            }
+            .sign_form{
+                border-width: 2px;
+                border-color: blue;
+            }
+        </style>
     </head>
     <body>
         <div class="navbar navbar-inverse navbar-fixed-top">
@@ -30,20 +38,10 @@
                 <a class="navbar-brand" href="#">Sunrise</a>
             </div>
         </div>
-        <form action="<?= $GLOBALS['sr_root'] ?>/models/user.php" name="signin_form" id="signin_form" method="post">
-            <input type="text" id="email" name="email" value="Email" /><br />
-            <input type="text" id="password" name="password" value="Password" /><br />
-            <input type="submit" value="Sign In" />
-=======
-
-    </head>
-    <body>
-        <h1>Sign In</h1>
-        <form action="<?= $GLOBALS['sr_root_husky'] ?>/controllers/signin.php" name="signin_form" id="signin_form" method="post">
+        <form class="sign_form" action="<?= $GLOBALS['sr_root_husky'] ?>/controllers/signin.php" name="signin_form" id="signin_form" method="post">
             <input type="text" id="email" name="email" placeholder="Email" /><br />
             <input type="password" id="password" name="password" placeholder="Password" /><br />
             <input type="submit" id="submit" name="submit" value="Sign In" />
->>>>>>> 49c0975519ea3e678d3909cbd4d74aa68a880073
         </form>
     </body>
 </html>
