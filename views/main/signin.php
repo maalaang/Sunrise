40f1420dd853a640b05700d43a66d6e62dbd4828
whale <whale@maalaang.com>
2013-08-09 15:39:28 +0900

    Merge

--- views/main/signin.php
+++ views/main/signin.php
@@ -46,7 +46,7 @@
         <div class="header">
             <div class="navbar navbar-inverse navbar-fixed-top">
                 <div class="container">
-                <a class="navbar-brand" href="<?= $GLOBALS['sr_root'] ?>/d/main/">Sunrise</a>
+                    <a class="navbar-brand" href="<?= $GLOBALS['sr_root'] ?>/d/main/">Sunrise</a>
                 </div>
             </div>
         </div>
