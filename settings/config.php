a22bd3a11ab73e96fc94a6dc9a715b2618274029
whale <whale@maalaang.com>
2013-08-09 13:52:26 +0900

    Merge

--- settings/config.php
+++ settings/config.php
@@ -12,7 +12,7 @@ $sr_db_charset      = 'utf8';
 /**
  * Sunrise VC home directory from the web server root.
  */
-$sr_root        = '/workspace/blackhat/Sunrise';
+$sr_root        = '/workspace/whale/Sunrise';
 
 
 /**
