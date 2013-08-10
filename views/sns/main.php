5ddf4e0472793a280c6338f9295fa7139b2b1b6a
Junsung Kim <jun3734@gmail.com>
2013-08-10 14:15:18 +0900

    send_message

--- views/sns/main.php
+++ views/sns/main.php
@@ -1,69 +1,13 @@
-<script src="http://connect.facebook.net/en_US/all.js"></script>
-
 <div id="fb-root"></div>
-
 <script>
-	window.fbAsyncInit = function() {
-
-		FB.init({
-
-			appId : '426582144117672',
-			channelUrl : 'http://dev.maalaang.com/workspace/brownbear/Sunrise/d/sns/test/',
-			status : true,
-			xfbml : true
-
-		});
-
-		FB.getLoginStatus(function(response) {
-
-			if (response.status === 'connected') {
-				var uid = response.authResponse.userID;
-				var accessToken = response.authResponse.accessToken;
-				console.log(accessToken);
-			} else if (response.status === 'not_authorized') {
-				console.log('not_authorized');
-			} else {
-				console.log('no login');
-			}
-		});
-
-		FB.login(function(response) {
-			if (response.authResponse) {
-
-				FB.api('/me', function(response) {
-					console.log('Good to see you, ' + response.name + '.');
-				});
-
-				var body = 'Sunrise SNS_facebook Test ';
-				FB.api('/me/feed', 'post', {
-					message : body
-				}, function(response) {
-					if (!response || response.error) {
-						console.log(response.error.code + ' ' + response.error.message);
-					} else {
-
-						console.log('Post ID: ' + response.id);
-					}
-				});
-			} else {
-			}
-		});
-
-		FB.login(function(response) {
-			if (response.authResponse) {
-				console.log('Welcome!  Fetching your information.... ');
-				FB.api('/me', function(response) {
-					console.log('Good to see you, ' + response.name + '.');
-				});
-			} else {
-				console.log('User cancelled login or did not fully authorize.');
-
-			}
-
-		}, {
-			scope : 'publish_stream'
-		});
-
-	};
-
+	( function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id))
+				return;
+			js = d.createElement(s);
+			js.id = id;
+			js.src = "//connect.facebook.net/ko_KR/all.js#xfbml=1&appId=426582144117672";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk')); 
 </script>
+<div class="fb-send" data-href="http://example.com"></div>
