7b7bd3c4fb10367850ef16d22f3efd78a7d6367f
whale <whale@maalaang.com>
2013-08-11 22:46:54 +0900

    Room Page (Chatting func ...ed)

--- views/room/room_proto.php
+++ views/room/room_proto.php
@@ -42,7 +42,18 @@
             function whenClickExit(){
             }
             function whenClickSend(){
-                $("#output_message").value = "asdf";
+                var input_message;
+                var output_message;
+
+                input_message = document.getElementById('input_message').value;
+                output_message = document.getElementById('output_message').value;
+                console.log(input_message);
+                console.log(output_message);
+                
+                output_message += input_message+'\n';
+                document.getElementById('input_message').value = '';
+                document.getElementById('output_message').value = output_message;
+                
             }
             //Initalizing JScrollpane
             $(document).ready(function(){
@@ -127,66 +138,11 @@
 
         <div class="side_content col-lg-4">
             <h1>Side_Content</h1>
-<!--                <div class="scroll-pane" style="width:100%; height:400px; overflow:auto;">
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in ligula id sem tristique ultrices
-                    eget id neque. Duis enim turpis, tempus at accumsan vitae, lobortis id sapien. Pellentesque nec orci
-                    mi, in pharetra ligula. Nulla facilisi. Nulla facilisi. Mauris convallis venenatis massa, quis
-                    consectetur felis ornare quis. Sed aliquet nunc ac ante molestie ultricies. Nam pulvinar ultricies
-                    bibendum. Vivamus diam leo, faucibus et vehicula eu, molestie sit amet dui. Proin nec orci et elit
-                    semper ultrices. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus
-                    mus. Sed quis urna mi, ac dignissim mauris. Quisque mollis ornare mauris, sed laoreet diam malesuada
-                    quis. Proin vel elementum ante. Donec hendrerit arcu ac odio tincidunt posuere. Vestibulum nec risus
-                    eu lacus semper viverra.
-                    </p>
-                    <p>
-                    Vestibulum dictum consectetur magna eu egestas. Praesent molestie dapibus erat, sit amet sodales
-                    lectus congue ut. Nam adipiscing, tortor ac blandit egestas, lorem ligula posuere ipsum, nec
-                    faucibus nisl enim eu purus. Quisque bibendum diam quis nunc eleifend at molestie libero tincidunt.
-                    Quisque tincidunt sapien a sapien pellentesque consequat. Mauris adipiscing venenatis augue ut
-                    tempor. Donec auctor mattis quam quis aliquam. Nullam ultrices erat in dolor pharetra bibendum.
-                    Suspendisse eget odio ut libero imperdiet rhoncus. Curabitur aliquet, ipsum sit amet aliquet varius,
-                    est urna ullamcorper magna, sed eleifend libero nunc non erat. Vivamus semper turpis ac turpis
-                    volutpat non cursus velit aliquam. Fusce id tortor id sapien porta egestas. Nulla venenatis luctus
-                    libero et suscipit. Sed sed purus risus. Donec auctor, leo nec eleifend vehicula, lacus felis
-                    sollicitudin est, vitae lacinia lectus urna nec libero. Aliquam pellentesque, arcu condimentum
-                    pharetra vestibulum, lectus felis malesuada felis, vel fringilla dolor dui tempus nisi. In hac
-                    habitasse platea dictumst. Ut imperdiet mauris vitae eros varius eget accumsan lectus adipiscing.
-                    </p>
-                    <p>
-                    Quisque et massa leo, sit amet adipiscing nisi. Mauris vel condimentum dolor. Duis quis ullamcorper
-                    eros. Proin metus dui, facilisis id bibendum sed, aliquet non ipsum. Aenean pulvinar risus eu nisi
-                    dictum eleifend. Maecenas mattis dolor eget lectus pretium eget molestie libero auctor. Praesent sit
-                    amet tellus sed nibh convallis semper. Curabitur nisl odio, feugiat non dapibus sed, tincidunt ut
-                    est. Nullam erat velit, suscipit aliquet commodo sit amet, mollis in mauris. Curabitur pharetra
-                    dictum interdum. In posuere pretium ultricies. Curabitur volutpat eros vehicula quam ultrices
-                    varius. Proin volutpat enim a massa tempor ornare. Sed ullamcorper fermentum nisl, ac hendrerit sem
-                    feugiat ac. Donec porttitor ullamcorper quam. Morbi pretium adipiscing quam, quis bibendum diam
-                    congue eget. Sed at lectus at est malesuada iaculis. Sed fermentum quam dui. Donec eget ipsum dolor,
-                    id mollis nisi. Donec fermentum vehicula porta.
-                    </p>
-                    <p>
-                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                    Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero
-                    sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
-                    Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed,
-                    commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros
-                    ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis.
-                    Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna
-                    eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis
-                    luctus, metus
-                    </p>
-                    <p>
-                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                    Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
-                    amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
-                    </p>
-            </div>-->
             <div class="chat-form">
                 <div class="form-group">
                     <textarea class="form-control" id="output_message" rows="20" text="asdfae"></textarea>
                     <form class="form-inline">
-                        <button type="button" class="btn btn-primary" style="float:right; width:20%;" onclick="whenClickSend">Send</button>
+                        <button type="button" class="btn btn-primary" style="float:right; width:20%;" onclick="whenClickSend()">Send</button>
                         <!--Send message to server-->
                         <!--<button type="button" class="btn btn-primary" style="float:right; width:20%;">Send</button>-->
                         <input type="text" class="form-control" id="input_message" placeholder="Input your message" style="width:80%;">
