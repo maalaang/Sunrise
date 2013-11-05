44db4ef73992aef1a67e038f5974f5453d58dae4
Sangwon Park <hudoni@gmail.com>
2013-11-05 17:40:10 +0900

    Upgraded bootstrap to version .3.0.1; Bug fixed - header collapse menu appears correctly on mobile browsers

--- views/header00.php
+++ views/header00.php
@@ -2,10 +2,10 @@
 <div class="navbar navbar-fixed-top">
     <div class="navbar-inner">
         <div class="container-fluid">
+            <a class="brand" href="<?= $GLOBALS['sr_root'] ?>/d/admin/dashboard/">Sunrise Admin Page</a>
             <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
                 <span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
             </a>
-            <a class="brand" href="<?= $GLOBALS['sr_root'] ?>/d/admin/dashboard/">Sunrise Admin Page</a>
             <div class="nav-collapse collapse">
                 <ul class="nav pull-right">
                     <li class="dropdown admin-user-menu">
@@ -30,3 +30,4 @@
         </div>
     </div>
 </div>
+
--- views/header01.php
+++ views/header01.php
@@ -1,14 +1,25 @@
 <!-- Header for Before Signin with Signup Button -->
-<div class="navbar navbar-inverse navbar-fixed-top">
+<header class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="<?= $GLOBALS['sr_root'] ?>/d/main/">Sunrise</a>
-        <div class="nav-collapse collapse pull-right">
-            <form class="navbar-form form-inline" action="<?= $GLOBALS['sr_root'] ?>/d/main/signin/" id="signin_form" name="signin_form" method="post">
-                <input type="text" class="form-control" id="signin_email" name="signin_email" placeholder="Email">
-                <input type="password" class="form-control" id="signin_password" name="signin_password" placeholder="Password">
-                <button type="button" class="btn btn-inverse" id="btn_signin" onclick="whenSignin(event)">Sign In</button>
-                <button type="button" class="btn btn-inverse" id="btn_signup" onclick="whenClickSignup(event)">Sign Up</button>
-            </form>
+        <div class="navbar-header">
+            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="<?= $GLOBALS['sr_root'] ?>/d/main/">Sunrise</a>
         </div>
+        <nav class="collapse navbar-collapse bs-navbar-collapse " role="navigation">
+            <ul class="nav navbar-nav pull-right">
+                <li>
+                    <form class="navbar-form form-inline" action="<?= $GLOBALS['sr_root'] ?>/d/main/signin/" id="signin_form" name="signin_form" method="post">
+                        <input type="text" class="form-control" id="signin_email" name="signin_email" placeholder="Email">
+                        <input type="password" class="form-control" id="signin_password" name="signin_password" placeholder="Password">
+                        <button type="button" class="btn btn-inverse" id="btn_signin" onclick="whenSignin(event)">Sign In</button>
+                        <button type="button" class="btn btn-inverse" id="btn_signup" onclick="whenClickSignup(event)">Sign Up</button>
+                    </form>
+                </li>
+            </ul>
+        </nav>
     </div>
-</div>
+</header>
