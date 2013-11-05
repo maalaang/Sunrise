f908eec0b14989c8098f7f9fef638c0007883859
Sangwon Park <hudoni@gmail.com>
2013-11-06 00:24:30 +0900

    View - headers: modified collapsed user menus on navbar for mobile browsers

--- views/header04.php
+++ views/header04.php
@@ -1,10 +1,25 @@
 <!-- Header for After Signin -->
-<div class="navbar navbar-inverse navbar-fixed-top">
+<header class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="<?= $GLOBALS['sr_root'] ?>/d/main/">Sunrise</a>
-        <div class="nav-collapse collapse pull-right">
-            <ul class="nav">
-                <li class="dropdown user-menu">
+        <div class="navbar-header">
+            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="<?= $GLOBALS['sr_root'] ?>/d/main/">Sunrise</a>
+        </div>
+        <nav class="collapse navbar-collapse bs-navbar-collapse " role="navigation">
+            <ul class="nav navbar-nav pull-right navbar-nav-small">
+                <li> <a tabindex="-1" href="#">My Profile</a> </li>
+                <? if (sr_is_admin()) { ?>
+                <li> <a tabindex="-1" href="<?= $GLOBALS['sr_root'] ?>/d/admin/">Admin Page</a> </li>
+                <? } ?>
+                <li class="divider"></li>
+                <li> <a tabindex="-1" href="<?= $GLOBALS['sr_root'] ?>/d/main/signout/">Sign Out</a> </li>
+            </ul>
+            <ul class="nav navbar-nav pull-right navbar-nav-large">
+                <li class="dropdown user-menu user-menu-large">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <i class="icon-user"></i>
                         <span class="user-name"><?= sr_user_name() ?></span>
@@ -20,6 +35,6 @@
                     </ul>
                 </li>
             </ul>
-        </div>
+        </nav>
     </div>
-</div>
+</header>
